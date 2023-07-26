b4471ca2fe05c9cc36f810ee16ac60a9aade5d94
iamthejahid2 <119108681+iamthejahid2@users.noreply.github.com>
2023-07-26 15:50:49 +0600

    First C Lab

---
+++ Untitled1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    /*char name[30];
+    printf("What's your name?\n");
+    scanf("%s", &name);
+    printf("Welcome %s\n", name);
+    if ( sizeof(name) < 6 ) {
+        printf("Btw, Your name is too short, %s", name);
+    } else {
+        printf("Your name has %lu characters.", sizeof(name));
+    } */
+
+    int a;
+    float b;
+    char c;
+    return 0;
+}
